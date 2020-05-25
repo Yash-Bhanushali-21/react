4b0920b3f56d16fd72d9ca5c791106df7fba4a50
yash11213018 <yash.bhanushali@sakec.ac.in>
2020-05-25 20:57:21 +0530

    Updates

---
+++ precache-manifest.4268e94d2fcb913cd9b703af44e192a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9383ffb3dc046babea8f9af0dc90adfb",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "e6dd34def8deade204c0",
+    "url": "/react/static/css/main.aaebdc0c.chunk.css"
+  },
+  {
+    "revision": "1841d4b34b065ff13cfa",
+    "url": "/react/static/js/2.039edc38.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react/static/js/2.039edc38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6dd34def8deade204c0",
+    "url": "/react/static/js/main.cfe82201.chunk.js"
+  },
+  {
+    "revision": "42239e0aedceb2f942fd",
+    "url": "/react/static/js/runtime-main.0382fa6a.js"
+  }
+]);
